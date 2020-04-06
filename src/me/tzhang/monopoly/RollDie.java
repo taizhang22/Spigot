ff258dca8736b70890e9dbc56aca6804c5c91f04
taizhang22 <41173438+taizhang22@users.noreply.github.com>
2020-04-06 11:18:27 -0700

    fixed and used version
    fixed by A.N.
    minor details with naimg schemes and clean-up.

--- src/me/tzhang/monopoly/RollDie.java
+++ src/me/tzhang/monopoly/RollDie.java
@@ -1,26 +1,22 @@
 package me.tzhang.monopoly;
 
-import org.bukkit.entity.Player;
-
 public class RollDie {
-        public static void main(String[] args) {
+
+    public static String roll(){
+
+        int total = 0;
+        String send = "Rolled: ";
+
+        for(int i = 0; i < 2; i++){
+            int rolled = (int)(Math.random() * 6) + 1;
+            send = send.concat(String.valueOf("|" + rolled + "| "));
+            total += rolled;
         }
+        send = send.concat("Total: " + total);
 
-        public static String roll(){
-
-            int total = 0;
-            String send = "Rolled: ";
-
-                for(int i = 0; i < 2; i++){
-                    int rolled = (int)(Math.random() * 6) + 1;
-                    send = send.concat(String.valueOf("|" + rolled + "| "));
-                    total += rolled;
-                }
-                send = send.concat("Total: " + total);
-
-                return send;
-
-        }
-
+        return send;
 
     }
+
+
+}
--- src/me/tzhang/monopoly/monopolyMain.java
+++ src/me/tzhang/monopoly/monopolyMain.java
@@ -2,20 +2,20 @@ package me.tzhang.monopoly;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class monopolyMain extends JavaPlugin implements Listener {
+public class monopolyMain extends JavaPlugin {
     private Commands commands = new Commands();
 
-        public void onEnable(){
-            getCommand(commands.cmd1).setExecutor(commands);
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "\n Monopoly plugin has been enabled! \n");
-        }
+    @Override
+    public void onEnable(){
+        getCommand(commands.cmd1).setExecutor(commands);
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Monopoly plugin has been enabled!");
+    }
 
-
+    @Override
     public void onDisable(){
-        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "\n Monopoly plugin has been disabled! \n");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Monopoly plugin has been disabled!");
     }
 
 }
